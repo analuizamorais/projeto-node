81fbf4e52629bc6732f0025ec26589f95de226b2
Ana Luiza Morais <80929132+analuizamorais@users.noreply.github.com>
2021-07-11 18:37:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const livros = require('./database')
+const [{id, nome, autor, categoria, paginas, recomenda, leu}] = livros
+//console.log(livros)
+
+//pegar o input 
+const readline = require('readline-sync')
+console.log("testeeeeeeeeeeeeeee")
+
+const entradaInicial = readline.question('Deseja buscar um livro? S/N')
+
+entradaInicial.toLocaleUpperCase() === "S" ? console.table(categoriasDisponiveis()) : console.table(exibirTodosOsLivros())
+
+function categoriasDisponiveis(){
+    console.log(`Essas são as categorias disponíveis: ${livros.map(livros => livros.categoria)}`)
+    const entradaCategoria = readline.question('Qual categoria você escolhe?')
+    console.log(entradaCategoria)
+
+    return livros.filter(livro => livro.categoria === entradaCategoria)
+}
+
+function exibirTodosOsLivros(){
+    console.log('Estes são todos os livros disponiveis:')
+    livros.sort((a, b) => a.paginas - b.paginas)
+    return livros.map(livros => livros)
+}
---
+++ database.js
@@ -0,0 +1,40 @@
+const livros = [
+    {
+        id: 1,
+        nome: "Para onde ela foi",
+        autor: "Gayle Forman",
+        categoria: "Romance",
+        paginas: 254,
+        recomenda: false,
+        leu: true
+    },
+    {
+        id: 2,
+        nome: "A morte é um dia que vale a pena viver",
+        autor: "Ana Claudia Abrantes",
+        categoria: "Saude",
+        paginas: 265,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 3,
+        nome: "O diário de Anne Frank",
+        autor: "Anne Frank",
+        categoria: "Historia",
+        paginas: 320,
+        recomenda: true,
+        leu: true
+    },
+    {
+        id: 4,
+        nome: "Um caminha para liberdade",
+        autor: "Jojo Moyes",
+        categoria: "Drama",
+        paginas: 230,
+        recomenda: true,
+        leu: false
+    }
+]
+
+module.exports = livros
